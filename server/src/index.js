3aa3207a4dc70bda400c93dfc934351b8c20ef0b
bleek42 <brandonleek42@gmail.com>
2020-09-22 19:29:25 -0400

    start over: init client with react/ts template && server with puppeteer, scrape function

---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();;
+const puppeteer = require('puppeteer');
+
+
+// (async () => {
+
+// })();
+
+const scrape = async url => {
+    // init the headless browser and a new page
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // await the url we want to go to
+    await page.goto(url);
+    // get element on page by Xpath, source property
+    const [elem] = await page.$x('//*[@id="imgBlkFront"]');
+    const src = await elem.getProperty('src');
+
+    const srcText = await src.jsonValue();
+    console.log({ srcText });
+
+};
+
+scrape(process.env.URL);
